14c28f4033867232cf61ef771b8b90d0edca2e83
Vaibhavkadam304 <vaibhav.kadam20@vit.edu>
2024-08-07 19:50:03 +0530

    first commit

--- app.py
+++ app.py
@@ -1,20 +1,48 @@
+# from flask import Flask, jsonify
+# from flask_cors import CORS
+# from pymongo import MongoClient
+# import os
+
+# app = Flask(__name__)
+# CORS(app)
+
+# # Use the online database URL
+# DATABASE_URL = 'mongodb+srv://vaibhav:chatrapati@cluster0.yvwdq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+# client = MongoClient(DATABASE_URL)
+
+# # Replace 'mydatabase' with the actual database name
+# db = client['mydatabase']
+
+# # Replace 'mycollection' with the actual collection name
+# collection = db['mycollection']
+
+# @app.route('/api/data', methods=['GET'])
+# def get_data():
+#     data = list(collection.find({}, {'_id': 0}))
+#     return jsonify(data)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
 from flask import Flask, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
-import os
 from dotenv import load_dotenv
+import os
 
+# Load environment variables from .env file
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
+# Use the database URL from environment variables
 DATABASE_URL = os.getenv('DATABASE_URL')
-PORT = int(os.getenv('PORT', 5000))
-
 client = MongoClient(DATABASE_URL)
 
+# Replace 'mydatabase' with the actual database name
 db = client['mydatabase']
+
+# Replace 'mycollection' with the actual collection name
 collection = db['mycollection']
 
 @app.route('/api/data', methods=['GET'])
@@ -23,4 +51,6 @@ def get_data():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    port = int(os.getenv('PORT', 5000))  # Default to port 5000 if not specified
+    app.run(debug=True, port=port)
+
